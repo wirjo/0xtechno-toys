eac8949e34005a656a498df8594336594323f827
Daniel Wirjo <daniel@wirjo.com>
2023-01-02 12:54:32 +1100

    fix: build error

--- packages/frontend/components/SectionMint.tsx
+++ packages/frontend/components/SectionMint.tsx
@@ -16,7 +16,7 @@ const SectionMint = (): JSX.Element => {
         <div className="container px-5 mx-auto max-w-screen-lg">
           <DappElement contract={nftContract}>
             <div className="text-center my-20">
-                <p className="text-5xl uppercase">Mint is now closed.</p><br/>Appreciate everyone's support and can't wait to explore the mints. Thank you!
+                <p className="text-5xl uppercase">Mint is now closed.</p><br/>Appreciate the support from everyone and cannot wait to explore the mints. Thank you!
                 <br/><br/>
                 <a className="underline" target="_blank" rel="noreferrer" href="https://twitter.com/0xTechno/status/1609727832984518656?cxt=HHwWgIDQ4e6789YsAAAA">Please your feedback and comments on Twitter.</a>
             </div>
