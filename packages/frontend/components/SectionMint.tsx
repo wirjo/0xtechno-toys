f7aff1db12bfbffee7c5ef48775c1c50b2a3ff7c
Daniel Wirjo <daniel@wirjo.com>
2023-01-02 17:01:50 +1100

    fix: sansa link

--- packages/frontend/components/SectionMint.tsx
+++ packages/frontend/components/SectionMint.tsx
@@ -1,4 +1,4 @@
-// import Mint from '../components/nft/Mint';
+import Mint from '../components/nft/Mint';
 import DappElement from '../components/DappElement';
 import NftArtifact from '../artifacts/contracts/Toy.sol/Toy.json';
 import { useContract } from '../hooks/useContract';
@@ -15,11 +15,7 @@ const SectionMint = (): JSX.Element => {
       <section id="section-mint" className="py-10">
         <div className="container px-5 mx-auto max-w-screen-lg">
           <DappElement contract={nftContract}>
-            <div className="text-center my-20">
-                <p className="text-5xl uppercase">Mint is now closed.</p><br/>Appreciate the support from everyone and cannot wait to explore the mints. Thank you!
-                <br/><br/>
-                <a className="underline" target="_blank" rel="noreferrer" href="https://twitter.com/0xTechno/status/1609727832984518656?cxt=HHwWgIDQ4e6789YsAAAA">Please your feedback and comments on Twitter.</a>
-            </div>
+            {nftContract ? <Mint contract={nftContract} /> : <></>}
           </DappElement>
         </div>
       </section>
